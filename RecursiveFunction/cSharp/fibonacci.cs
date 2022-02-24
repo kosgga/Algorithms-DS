8bf9f68b7c72595ad8f2671f1fadbb4aba0bdc54
Cozer <cozerr@gmail.com>
2022-02-24 17:02:32 +0300

    recursive fib cs added

---
+++ RecursiveFunction/cSharp/fibonacci.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Fib
+{
+    class program
+    {
+        static int fib(int n)
+        {
+            if (n==0)
+                return 0;
+            if (n==1 || n==2)
+                return 1;
+            else 
+                return(fib(n-1)+fib(n-2));
+        }
+        static void Main()
+        {
+            int n=5;
+            Console.WriteLine("5 satısının fibonacci serisi :");
+
+            for (int i=0;i<n,i++)
+            {
+                Console.WriteLine(fib(i)+" ");
+            }
+        }
+    }
+}
